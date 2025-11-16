d00f157fda9b2b49826e443c1b60cad6edd26189
PalmNeko <nitunamoni13579@gmail.com>
2025-11-17 06:54:05 +0900

    add: DoorController

---
+++ unityModule06/Assets/Scripts/DoorController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public BoxCollider doorWingCollider;
+    public Animator animator;
+    public GameObject pivot;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
